14f7e76e696cb00ca077de78a7511bdafa6d5409
xuxiaolong <xuxiaolong200691@163.com>
2019-10-06 17:19:47 +0800

    second

--- vue.config.js
+++ vue.config.js
@@ -19,9 +19,9 @@ module.exports = {
     filenameHashing: false,
 
     //   lintOnSave：{ type:Boolean default:true } 问你是否使用eslint
-    lintOnSave: true,
+    // lintOnSave: true,
     //如果你想要在生产构建时禁用 eslint-loader，你可以用如下配置
-    // lintOnSave: process.env.NODE_ENV !== 'production',
+    lintOnSave: process.env.NODE_ENV !== 'production',
 
     //是否使用包含运行时编译器的 Vue 构建版本。设置为 true 后你就可以在 Vue 组件中使用 template 选项了，但是这会让你的应用额外增加 10kb 左右。(默认false)
     // runtimeCompiler: false,
@@ -56,5 +56,8 @@ module.exports = {
                 target: "<other_url>"
             }
         }
+    },
+    configureWebpack: {
+
     }
 };
